76e9bf3c3c5a1d05b7d7298f982aaf215c7c61b1
Larissa Dantier <larissa_dantier@hotmail.com>
2021-03-14 22:32:19 -0300

    Conditions = Styles and list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,111 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, FlatList } from 'react-native';
 
 import Menu from '../../components/Menu';
 import Header from '../../components/Header';
 import Conditions from '../../components/Conditions'
+import Forcast from '../../components/Forcast'
+
+const mylist = [
+    {
+      "date": "13/03",
+      "weekday": "Sáb",
+      "max": 30,
+      "min": 22,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "14/03",
+      "weekday": "Dom",
+      "max": 31,
+      "min": 21,
+      "description": "Tempestades isoladas",
+      "condition": "storm"
+    },
+    {
+      "date": "15/03",
+      "weekday": "Seg",
+      "max": 31,
+      "min": 22,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "16/03",
+      "weekday": "Ter",
+      "max": 31,
+      "min": 22,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "17/03",
+      "weekday": "Qua",
+      "max": 31,
+      "min": 21,
+      "description": "Ensolarado",
+      "condition": "clear_day"
+    },
+    {
+      "date": "18/03",
+      "weekday": "Qui",
+      "max": 31,
+      "min": 21,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "19/03",
+      "weekday": "Sex",
+      "max": 30,
+      "min": 22,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "20/03",
+      "weekday": "Sáb",
+      "max": 30,
+      "min": 23,
+      "description": "Ensolarado com muitas nuvens",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "21/03",
+      "weekday": "Dom",
+      "max": 31,
+      "min": 22,
+      "description": "Ensolarado com muitas nuvens",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "22/03",
+      "weekday": "Seg",
+      "max": 31,
+      "min": 22,
+      "description": "Ensolarado com muitas nuvens",
+      "condition": "cloudly_day"
+    }
+  ];
 
 export default function Home(){
     return(
         <SafeAreaView style={styles.container}>
             <Menu/>
+            
             <Header/>
+
             <Conditions/>
+
+            <FlatList
+            horizontal={true} 
+            contentContainerStyle={{paddingBottom: '5%'}}
+            style={styles.list}
+            data={mylist} 
+            keyExtractor={item => item.date}
+            renderItem = { ({ item }) => <Forcast data={item}/> }
+            />
         </SafeAreaView>
     )
 }
@@ -22,5 +117,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor:'#e8f0ff',
         paddingTop: '5%'
+    },
+    list:{
+        marginTop: 10,
+        marginLeft: 10,
     }
 })
