e778e1c5823ab31f8d41d053b8178278813bec7e
Larissa Dantier <larissa_dantier@hotmail.com>
2021-03-16 22:33:38 -0300

    Seach - API

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,16 +1,37 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput, Keyboard } from 'react-native';
 
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import api, {key} from '../../services/api';
 
 export default function Search(){
-
+    const navigation = useNavigation();
     const[input, setInput] = useState('');
+    const[city, setCity] = useState(null);
+    const[error, setError] = useState(null);
+
+    async function handleSeach() {
+        const response = await api.get(`/weather?key=${key}&city_name=${input}`);
+
+        if(response.data.by === 'default'){
+            setError('Humm, cidade não encontrada!');
+            setInput('');
+            setCity(null);
+            Keyboard.dismiss();
+            return;
+        }
+
+        setCity(response.data);
+        setInput('');
+        Keyboard.dismiss();
+    }
 
     return(
         <SafeAreaView style={styles.container}>
 
-        <TouchableOpacity style={styles.backButton}>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('Home')}>
             <Feather
             name="chevron-left"
             size={32}
@@ -26,7 +47,7 @@ export default function Search(){
             placeholder="Ex: Campos Dos Goytacazes, RJ"
             style={styles.input}
             />
-            <TouchableOpacity style = {styles.icon}>
+            <TouchableOpacity style = {styles.icon} onPress={handleSeach}>
                 <Feather
                 name='search'
                 size={22}
@@ -34,6 +55,8 @@ export default function Search(){
                 />
             </TouchableOpacity>
         </View>
+
+        {error && <Text style={{marginTop: 25, fontSize: 18}}>{error}</Text>}
 
         </SafeAreaView>
     )
