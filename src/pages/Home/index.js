50a5493879256d7d658b6fc3a8ade681f4746484
Larissa Dantier <larissa_dantier@hotmail.com>
2021-03-16 21:24:39 -0300

    New Past - Services - New File - api.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { SafeAreaView, Text, StyleSheet, FlatList } from 'react-native';
+import * as Location from 'expo-location';
 
 import Menu from '../../components/Menu';
 import Header from '../../components/Header';
 import Conditions from '../../components/Conditions'
 import Forcast from '../../components/Forcast'
+import api, { key } from '../../services/api'
+
 
 const mylist = [
     {
@@ -89,6 +92,52 @@ const mylist = [
     }
   ];
 export default function Home(){
+  const[errorMsg, setErrorMsg] = useState(null);
+  const[loading, setLoading] = useState(true);
+  const[weather, setWeather] = useState([]);
+  const [icon, setIcon] = useState({name: 'cloud', color:'#FFF'});
+  const [background, setBackground] = useState(['#1ed6ff', '#97c1ff']);
+
+
+  useEffect(() => {
+        
+    (async () => {
+      let {status} = await Location.requestPermissionsAsync();
+
+      if(status != 'granted'){
+        setErrorMsg('Permissão negada para acessar localização');
+        setLoading(false);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      
+      //weather?key=71abd0c6&lat=-21.7526009&lon=-41.3438487
+       const responde = await api.get(`/weather?key=${key}&lat=${location.coords.latitude}&lon=${location.coords.longitude}`);
+
+       setWeather(responde.data);
+
+       if(responde.data.results.currently === 'noite') {
+          setBackground(['#0c3741', '#0f2f61']);
+       }
+
+       switch(responde.data.results.condition_slug){
+         case 'clear_day':
+           setIcon({name: 'partly-sunny', color:'#FFB300'});
+           break;
+         case 'rain':
+           setIcon({name: 'rainy', color :'#FFF'});
+           break;
+         case 'storm':
+           setIcon({name:'rainy', color: '#FFF'})
+           break;
+       }
+
+       setLoading(false)
+      })();
+    }, []);
+
+
     return(
         <SafeAreaView style={styles.container}>
             <Menu/>
